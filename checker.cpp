7732e2bf0e6416fbcae224bea0a5a419268fa497
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-08-27 16:10:08 +0530

    Any vital check

--- checker.cpp
+++ checker.cpp
@@ -1,33 +1,17 @@
 #include <assert.h>
 
-const int bpmLimits[] = { 70,150 };
-const int respRateLimits[] = { 30,95 };
-const int spo2Limit = 90;
-
-bool bpmIsOk(float bpm)
+bool vitalIsOk(float value, int lower, int upper)
 {
-    return (bpm > bpmLimits[0] && bpm < bpmLimits[1]);
+    return (value > lower && value < upper);
 }
 
-bool spo2IsOk(float spo2)
-{
-    return spo2 > spo2Limit;
-}
-
-bool respRateIsOk(float respRate)
-{
-    return (respRate > respRateLimits[0] && respRate < respRateLimits[1]);
-}
 
 int main() {
-    assert(bpmIsOk(60) == false);
-    assert(bpmIsOk(90) == true);
-    assert(bpmIsOk(160) == false);
 
-    assert(spo2IsOk(100) == true);
-    assert(spo2IsOk(80) == false);
-
-    assert(respRateIsOk(20) == false);
-    assert(respRateIsOk(60) == true);
-    assert(respRateIsOk(160) == false);
+    assert(vitalIsOk(4,3,7) == true);
+    assert(vitalIsOk(70, 10, 20) == false);
+    assert(vitalIsOk(5, 10, 20) == false);
+    
+    return 0;
 }
+
