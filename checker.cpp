f2ff3af9b9eee83e8c948c31438e3b26673f156c
SoniaVadhnani <68679226+SoniaVadhnani@users.noreply.github.com>
2020-08-29 18:30:23 +0530

    decoupling concept

--- checker.cpp
+++ checker.cpp
@@ -1,51 +1,135 @@
-#include <assert.h>
+#include<iostream>
+#include<vector>
+#include<assert.h>
+using namespace std;
 
-const int bpmLimits[] = { 70,150 };
-const int respRateLimits[] = { 30,95 };
-const int spo2Limit[] = { 90,101 };
 
-bool vitalIsOk(float value, int lower, int upper)
+//--------------Alert---------------------------------
+class Alert
 {
-    return (value > lower && value < upper);
-}
+public:
+	virtual void alert(string alert_message) = 0;
+};
 
 
-bool bpmIsOk(float bpm)
+class AlertBySMS :public Alert
 {
-    return (bpm > bpmLimits[0] && bpm < bpmLimits[1]);
-}
+public:
+	void alert(string alert_message) override
+	{
+		cout << "Message : " << alert_message << endl;
+	}
+};
 
-bool spo2IsOk(float spo2)
+class AlertBySound :public Alert
 {
-    return spo2 > spo2Limit[0] && spo2 < spo2Limit[1];
-}
+public:
+	void alert(string alert_message) override
+	{
+		cout << "Sound : " << alert_message<<endl;
+	}
+};
 
-bool respRateIsOk(float respRate)
+//----------Accesing to decide the alert type------------------
+class Staff
 {
-    return (respRate > respRateLimits[0] && respRate < respRateLimits[1]);
+	Alert* alerttype;
+public:
+	void setAlert(Alert* type)
+	{
+		alerttype = type;
+	}
+
+	void AlerttheConcerned(string theMessage)
+	{
+		alerttype->alert(theMessage);
+	}
+};
+
+Staff staff;		// declaring global
+
+
+//------------List of the vitals----------------------------------
+struct vital
+{
+	string vital_name;
+	float min_Limit;
+	float max_Limit;
+};
+
+vector<vital> ListOfVitals;
+
+void addVital(string vitalName, float minlimit, float maxlimit)
+{
+	ListOfVitals.push_back({vitalName, minlimit, maxlimit });				//important the curly braces
 }
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-
-    return (vitalIsOk(bpm, bpmLimits[0], bpmLimits[1]) && vitalIsOk(spo2, spo2Limit[0], spo2Limit[1]) && vitalIsOk(respRate, respRateLimits[0], respRateLimits[1]));
-
+int searchVitalPosition(string vitalName)
+{
+	int i;
+	for ( i=0; i < ListOfVitals.size(); i++)
+	{
+		if (ListOfVitals[i].vital_name == vitalName)
+			break;
+	}
+	return i;
 }
 
-int main() {
-    assert(bpmIsOk(60) == false);
-    assert(bpmIsOk(90) == true);
-    assert(bpmIsOk(160) == false);
+//------------vital is ok--------------------
+bool vitalIsOk(string vitalname, float value)
+{
+	int position = searchVitalPosition(vitalname);
+	if (value < ListOfVitals[position].min_Limit)
+	{
+		staff.AlerttheConcerned(vitalname + " is low!");
+		return false;
+	}
+	if (value > ListOfVitals[position].max_Limit)
+	{
+		staff.AlerttheConcerned(vitalname + " is high!");
+		return false;
+	}
+	return true;
+}
 
-    assert(spo2IsOk(100) == true);
-    assert(spo2IsOk(80) == false);
 
-    assert(respRateIsOk(20) == false);
-    assert(respRateIsOk(60) == true);
-    assert(respRateIsOk(160) == false);
 
-    assert(vitalIsOk(4, 3, 7) == true);
-    assert(vitalIsOk(70, 10, 20) == false);
-    assert(vitalIsOk(5, 10, 20) == false);
+int main()
+{
 
-    return 0;
+	addVital("BPM", 70, 150);
+	addVital("SPO2", 90, 101);
+	addVital("RESPRATE", 30, 95);
+
+	AlertBySMS bysms;
+	AlertBySound bysound;
+
+	staff.setAlert(&bysms);
+
+	assert(vitalIsOk("BPM", 60)==false);
+	assert(vitalIsOk("BPM", 160) == false);
+	assert(vitalIsOk("BPM", 80) == true);
+
+	assert(vitalIsOk("SPO2", 60) == false);
+	assert(vitalIsOk("SPO2", 110) == false);
+	assert(vitalIsOk("SPO2", 95) == true);
+
+	assert(vitalIsOk("RESPRATE", 20) == false);
+	assert(vitalIsOk("RESPRATE", 110) == false);
+	assert(vitalIsOk("RESPRATE", 60) == true);
+
+	staff.setAlert(&bysound);
+
+	assert(vitalIsOk("BPM", 60) == false);
+	assert(vitalIsOk("BPM", 160) == false);
+	assert(vitalIsOk("BPM", 80) == true);
+
+	addVital("BP", 80, 120);
+
+	assert(vitalIsOk("BP", 60) == false);
+	assert(vitalIsOk("BP", 160) == false);
+	assert(vitalIsOk("BP", 90) == true);
+
+
+	return 0;
 }
